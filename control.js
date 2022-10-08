20b5db98d5a56186aef4aade4b88c20e23f625f7
Adam Mery <adam@mery.online>
2022-10-08 10:53:46 +0200

    self_driving_car

---
+++ control.js
@@ -0,0 +1,52 @@
+class Controls {
+	constructor() {
+		this.fowrad = false;
+		this.left = false;
+		this.right = false;
+		this.reverse = false;
+
+		this.#addKeyboardListeners();
+	}
+
+	#addKeyboardListeners () {
+		document.onkeydown = (event) => {
+			switch (event.key) {
+				case "ArrowLeft":
+					this.left = true;
+					break;
+
+				case "ArrowRight":
+					this.right = true;
+					break;
+
+				case "ArrowUp":
+					this.fowrad = true;
+					break;
+
+				case "ArrowDown":
+					this.reverse = true;
+					break;
+			}
+		}
+
+		document.onkeyup = (event) => {
+			switch (event.key) {
+				case "ArrowLeft":
+					this.left = false;
+					break;
+
+				case "ArrowRight":
+					this.right = false;
+					break;
+
+				case "ArrowUp":
+					this.fowrad = false;
+					break;
+
+				case "ArrowDown":
+					this.reverse = false;
+					break;
+			}
+		}
+	}
+}
